4e9a564f308550bdd84b265ffe702c2d9bcc8c8a
sophoraFlower <2268228773@qq.com>
2021-09-02 23:18:42 +0800

    advanced-java Filter 0902

--- src/cn/advanced/java/example01/LoginFilter.java
+++ src/cn/advanced/java/example01/LoginFilter.java
@@ -5,13 +5,20 @@ import java.io.IOException;
 
 public class LoginFilter implements Filter {
 
-    private static final String LOGIN_URL = "login_uri";
+    private static final String LOGIN_URI = "login_uri";
     public static final String HOME_URI = "home_uri";
     private String login_page;
     private String home_page;
-    
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+        // 从部署描述符中获取登录页面和首页的URI
+        login_page = filterConfig.getInitParameter(LOGIN_URI);
+        home_page = filterConfig.getInitParameter(HOME_URI);
+
+        if (null == login_page) {
+            throw new ServletException("没有指定登录页面或主页");
+        }
     }
 
     @Override
---
+++ web/home.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>首页</title>
+</head>
+<body>
+    ${sessionScope.user}，欢迎你～
+</body>
+</html>
