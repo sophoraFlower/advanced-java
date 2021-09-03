c96031933b0f27ffeacc9e9002a4c93316c54e46
sophoraFlower <2268228773@qq.com>
2021-09-04 02:39:06 +0800

    advanced-java Servlet示例 0904

--- src/cn/advanced/java/example02/WelcomeServlet.java
+++ src/cn/advanced/java/example02/WelcomeServlet.java
@@ -1,4 +1,41 @@
 package cn.advanced.java.example02;
 
-public class WelcomeServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class WelcomeServlet extends HttpServlet {
+
+    private String greeting;
+
+    @Override
+    public void init() {
+        greeting = getInitParameter("greeting");
+        // greeting = getServletConfig().getInitParameter("greeting");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("gb2312");
+        String username = req.getParameter("username");
+        String welcomeInfo = greeting + ", " + username;
+
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<html><head><title>");
+        out.println("Welcome page!");
+        out.println("</title></head>");
+        out.println("<body>");
+        out.println(welcomeInfo);
+        out.println("</body></html>");
+        out.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
 }
