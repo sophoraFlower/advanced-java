31f9046f180e95da5781f2ef8c44a0668acb758c
sophoraFlower <2268228773@qq.com>
2021-12-06 23:00:47 +0800

    Demo mybatis_ResultMap_demo 2021/12/06

---
+++ Demo/mybatis_ResultMap_demo/src/main/java/com/mialab/mybatis_ResultMap_demo/mapper/StudentMapper.java
@@ -0,0 +1,11 @@
+package com.mialab.mybatis_ResultMap_demo.mapper;
+
+import java.util.List;
+
+import com.mialab.mybatis_ResultMap_demo.domain.Student;
+
+public interface StudentMapper {
+	
+	public List<Student> getStudentAll();
+
+}
