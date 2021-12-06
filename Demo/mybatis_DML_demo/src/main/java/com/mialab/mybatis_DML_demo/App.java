eb6eaef8d1c8bcdaeb2263b2b6ff421b97d3fb69
sophoraFlower <2268228773@qq.com>
2021-12-06 23:00:21 +0800

    Demo mybatis_DML_demo 2021/12/06

---
+++ Demo/mybatis_DML_demo/src/main/java/com/mialab/mybatis_DML_demo/App.java
@@ -0,0 +1,13 @@
+package com.mialab.mybatis_DML_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
