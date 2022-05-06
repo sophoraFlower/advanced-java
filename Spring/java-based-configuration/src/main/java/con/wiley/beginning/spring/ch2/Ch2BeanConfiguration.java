c8b3dad28780c4f53338d504afb603bc58a9835a
Houle <houle.cf@foxmail.com>
2022-05-06 22:25:23 +0800

    Project：java-based-configuration

---
+++ Spring/java-based-configuration/src/main/java/con/wiley/beginning/spring/ch2/Ch2BeanConfiguration.java
@@ -0,0 +1,22 @@
+package con.wiley.beginning.spring.ch2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Ch2BeanConfiguration {
+
+    @Bean
+    public AccountService accountService() {
+        AccountServiceImpl bean = new AccountServiceImpl();
+        bean.setAccountDao(accountDao());
+        return bean;
+    }
+
+    @Bean
+    public AccountDao accountDao() {
+        AccountDaoInMemoryImpl bean = new AccountDaoInMemoryImpl();
+        //depedencies of accountDao bean will be injected here...
+        return bean;
+    }
+}
