0a36d7f53520954d5c3af0f95698351a8282a764
Houle <houle.cf@foxmail.com>
2022-05-12 22:38:12 +0800

    Java Spring 2022/05/21

---
+++ Spring/ch2/src/main/java/cn/osxm/ssmi/dao/HelloDao.java
@@ -0,0 +1,7 @@
+package cn.osxm.ssmi.dao;
+
+public class HelloDao {
+    public void insert() {
+        System.out.println("Hello Dao, insert");
+    }
+}
---
+++ Spring/ch2/src/main/java/cn/osxm/ssmi/dao/UserDao.java
@@ -0,0 +1,7 @@
+package cn.osxm.ssmi.dao;
+
+import cn.osxm.ssmi.pojo.User;
+
+public interface UserDao {
+    void add(User user);
+}
